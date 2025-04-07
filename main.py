a383b5584099de3eb043d483b16fcf201af47aaf
Ranjan000001 <ranjan.chau14@gmail.com>
2025-04-08 01:52:04 +0530

    first code

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__'
+    import pandas as pd
+    import numpy as np
+    arr=np.array([1, 2, 4, 5, 6, 9, 10])
+    df=pd.DataFrame({'colum1': arr})
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
